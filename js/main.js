178865a715e1e0cb5d755804401f8e71c90d1cf7
Von Matthew Alfafara <valfafara@yondu.com>
2019-07-27 22:13:02 +0800

    Update design

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 
-window.onload = function () {
+window.onload = () => {
+   document.body.classList.remove('preload')
+
    const menu = document.querySelector('.menu')
    const line1 = document.querySelector('.line:nth-child(1)')
    const line2 = document.querySelector('.line:nth-child(2)')
    const line3 = document.querySelector('.line:nth-child(3)')
    const nav = document.querySelector('nav')
+   const logo = nav.querySelector('.logo')
+
+   const checkWindowWidth = () => {
+      if (window.innerWidth < 768)
+         logo.style.display = "none"
+      else
+         logo.style.display = "block"
+
+      console.log('checking width')
+   }
+
+   checkWindowWidth()
 
    menu.addEventListener('click', () => {
       line1.classList.toggle('rotatefirst')
@@ -13,4 +27,15 @@ window.onload = function () {
       nav.classList.toggle('show')
       document.body.classList.toggle('overflowYhidden')
    })
+
+   window.onscroll = () => {
+      if (document.documentElement.scrollTop)
+         nav.classList.add('background-transparent-black')
+      else
+         nav.classList.remove('background-transparent-black')
+   }
+
+   window.onresize = () => {
+      checkWindowWidth()
+   }
 }
